adaed18adcfd063fa85e5e10ac67836e05326bd8
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2026-04-01 11:30:09 -0700

    Added Dashboard view to validate routing is functional

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,7 +26,17 @@ namespace PlanetCrafterAssistant.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(
+                new ErrorViewModel
+                {
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                }
+            );
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
         }
     }
 }
