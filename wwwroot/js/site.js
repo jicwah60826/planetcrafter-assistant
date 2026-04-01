e59592405ceb970873da5d7359c4bfa204275ef3
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2026-04-01 13:39:17 -0700

    Updated search and added function for filterCards

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,14 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+<script>
+    function filterCards() {
+        let input = document.getElementById("searchBox").value.toLowerCase();
+    let cards = document.getElementsByClassName("card");
+
+    for (let i = 0; i < cards.length; i++) {
+        let text = cards[i].innerText.toLowerCase();
+    cards[i].style.display = text.includes(input) ? "" : "none";
+        }
+    }
+</script>
