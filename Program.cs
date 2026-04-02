88b153e084447cc62841862ebda575bb24194679
DESKTOP-J3LP9F6\jWhisman <jon@pixelgraft.com>
2026-04-01 17:06:05 -0700

    Changed default route action from Index → Recipes so / now loads the Recipes page. Removed the Resources and Tools nav links

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,8 +19,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Recipes}/{id?}");
 
 app.Run();
